36ea81ae58446de1c6d5412d1e95d33c1951ada4
niravchit <niravc10@gmail.com>
2017-12-20 14:08:59 -0800

    Titanic Kaggle Competition
    Competition file

---
+++ titanic_kaggle/titanic.py
@@ -0,0 +1,11 @@
+"""
+Goal: to predict if a passenger survived the sinking of the Titanic or not. Statistically speaking, women, children and upper class had best
+chance to survive. For each passenger in test set, you must predict 0 or 1 for the Survived variable
+
+Submission: 418 entries plus a header row. 2 columns - PassengerID, Survived
+
+"""
+import pandas
+import matplotlib as mpl
+import seaborn
+
