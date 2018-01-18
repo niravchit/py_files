bff19be0f78c9fec0d57d50a251148ec85f2c15b
niravchit <niravc10@gmail.com>
2018-01-17 16:48:54 -0800

    Random changes

--- titanic_kaggle/titanic.py
+++ titanic_kaggle/titanic.py
@@ -8,4 +8,21 @@ Submission: 418 entries plus a header row. 2 columns - PassengerID, Survived
 import pandas
 import matplotlib as mpl
 import seaborn
+import scipy
+from sklearn.linear_model import LinearRegression
+import numpy as np 
 
+
+# def velocity(t):
+#     return (25 + 10.0*t)
+# #integrate velocity to get distance
+# distance = scipy.integrate.quad(velocity, 0, 3)
+# print(distance)
+
+print('hello')
+#print(type(lambda x: x+1))
+
+r = np.arange(36)
+r.resize(6,6)
+print(r.reshape(36))
+print(r[2:4,2:4])
